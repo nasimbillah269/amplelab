67c6239dc0cd960e1c670aa6dde0dd214b8c2268
Nasim Billah <nasimbillah269@gmail.com>
2026-09-08 18:13:10 +0600

    error update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,11 +34,21 @@ class AppServiceProvider extends ServiceProvider
         \Config::set("services.google.client_secret", general()->google_client_secret);
         \Config::set("services.google.redirect", general()->google_client_redirect_url);
         
-        \Config::set("mail.mailers.smtp.transport", general()->mail_driver);
-        \Config::set("mail.mailers.smtp.host", general()->mail_host);
-        \Config::set("mail.mailers.smtp.port", general()->mail_port);
-        \Config::set("mail.mailers.smtp.encryption", general()->mail_encryption);
-        \Config::set("mail.mailers.smtp.username", general()->mail_username);
-        \Config::set("mail.mailers.smtp.password", general()->mail_password);
+        // Only override the mail config when SMTP is actually configured in
+        // the admin settings. Otherwise a null host makes the Symfony mailer
+        // throw "Argument #2 ($host) must be of type string, null given".
+        if (!empty($general->mail_host)) {
+            \Config::set("mail.mailers.smtp.transport", $general->mail_driver ?: 'smtp');
+            \Config::set("mail.mailers.smtp.host", $general->mail_host);
+            \Config::set("mail.mailers.smtp.port", $general->mail_port ?: 587);
+            \Config::set("mail.mailers.smtp.encryption", $general->mail_encryption ?: null);
+            \Config::set("mail.mailers.smtp.username", $general->mail_username);
+            \Config::set("mail.mailers.smtp.password", $general->mail_password);
+
+            if ($general->mail_from_address) {
+                \Config::set("mail.from.address", $general->mail_from_address);
+                \Config::set("mail.from.name", $general->mail_from_name ?: $general->title);
+            }
+        }
     }
 }
